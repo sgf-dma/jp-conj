76f91f13f6000bd5057a557cce3377e6045abf21
sgf <sgf.dma@gmail.com>
2018-06-27 13:17:46 +0300

    save

--- update-vforms-www.hs
+++ update-vforms-www.hs
@@ -4,14 +4,40 @@
  --resolver lts-11.3
  --package shelly
  --package pandoc
+ --package text
+ --package system-filepath
  -}
 {-# LANGUAGE OverloadedStrings  #-}
 
-import Shelly
+import qualified Shelly     as S
+import qualified Data.Text  as T
+import Data.Maybe
+import Control.Monad
+import Filesystem.Path.CurrentOS
 
+-- | Match filename according to expected "verb-forms" config file names:
+-- "verbs-mX.yaml", where X is lesson number.
+vformsConfigs :: S.FilePath -> S.Sh Bool
+vformsConfigs p = do
+    let (fp, mext) = splitExtension p
+    t <- S.toTextWarn (basename fp)
+    return ("verbs-m" `T.isPrefixOf` t && fromMaybe False ((== "yaml") <$> mext))
 
 main :: IO ()
 main        = do
-    shelly $ do
-      run "k2csv-exe" ["--config", "verbs-m14.yaml"]
+    S.shelly $ do
+      cfs <- S.findWhen vformsConfigs "."
+      S.liftIO $ mapM_ print cfs
+      -- Regenerate all vforms files.
+      S.mkdir_p "vforms"
+      forM_ cfs $ \cf -> do
+        ct <- S.toTextWarn cf
+        S.run_ "verb-forms" ["--config", ct]
+      --  Update conjugations.html .
+      S.run_ "stack" ["exec", "pandoc", "--"
+                     , "-s", "--template=default.html5"
+                     , "-f", "markdown+grid_tables"
+                     , "-o", "vforms/conjugations.html"
+                     ]
       return ()
+
